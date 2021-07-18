5b7b2b49894d039e8747300c2af9e7dc9aebf9f2
malavika16 <47278266+malavika16@users.noreply.github.com>
2021-07-18 12:42:48 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<div class="footer" id="footer">
+  <div class="container">
+    <p>Copyright © ABC Bank (Task1). All Rights Reserved | Contact Us: +91 90000 00000</p>
+  </div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+ <div class="container">
+ <div class="navbar-header">
+ <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+ <span class="icon-bar"></span>
+ <span class="icon-bar"></span>
+ <span class="icon-bar"></span>
+ </button>
+ <a class="navbar-brand" href="index.php">ABC Bank</a>
+ </div>
+ <div class="collapse navbar-collapse" id="myNavbar">
+ <ul class="nav navbar-nav navbar-right">
+   <li><a href="customers.php"><span class="glyphicon glyphicon-user"></span>
+   Customers</a></li>
+   <li><a href="transactions.php"><span class="glyphicon glyphicon-tasks"></span>
+   Transactions</a></li>
+</ul>
+</div>
+</div>
+</div>
